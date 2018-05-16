464d9575b0c873a40eeda9029bbbf012b4dcfa64
沈骏 <shenjuneng@sina.com>
2018-05-16 09:28:59 +0800

    Demo2第1次修改

--- StoryBoardGitDemo/StoryBoardGitDemo/AppDelegate.m
+++ StoryBoardGitDemo/StoryBoardGitDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // Demo2第1次修改
     return YES;
 }
 
